d8f97224cc16f0083c582d8d9547dace6f85b26c
chaymaemerhrioui1 <128318349+chaymaemerhrioui1@users.noreply.github.com>
2023-05-20 16:01:35 +0200

    Update Echiquierch.java

--- game/classes/Echiquier.java
+++ game/classes/Echiquier.java
@@ -1,12 +1,17 @@
 package com.game.classes;
 
+import java.awt.*;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import com.game.tools.Point;
 
 public class Echiquier {
+    private static final Logger logger = Logger.getLogger(Echiquier.class.getName());
+
     private static Echiquier echiquier;
     //crée une liste vide appelée "player1" qui contiendra les pièces du joueur 1.
     private List<Piece> player1 = new ArrayList<Piece>();
@@ -17,15 +22,27 @@ public class Echiquier {
     private List<Point> listPiegePlayer2 = new ArrayList<Point>();
     //crée une liste vide appelée "listWaterPlace" qui contiendra les emplacements des points d'eau sur le plateau.
     private List<Point> listWaterPlace = new ArrayList<Point>();
+    private Connection con;
+    private Statement st;
 
-    /**
-     * TODO ajuter les places du trone
-     */
+     // il faut instancier les postion ici dans le constructeur
 
-    /**
-     * il faut instancier les postion ici dans le constructeur
-     */
-    private Echiquier() {
+    public Echiquier() {
+        try {
+            // apeel au Driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // con pour se connecter a ma base de donnee(jeu xou dou qi contenat les tables necessaires
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeu xou dou qi", "root", "");
+            // creer statement
+            st = con.createStatement();
+            // si vous etes connectes on affiche message de bienvenue
+            System.out.println("*********************************Bienvenue <_>,votre connection est bien connecte !! ******************************************");
+        } catch (Exception ex) {
+            logger.severe("********** Notre exception s'est produite  ********** : " + ex.getMessage());
+            ex.printStackTrace();
+            // sinon en declanche un erreur
+            //System.out.println("Error" +ex);
+}
         //crée une nouvelle pièce "lionB" en utilisant la classe "Lion", en lui attribuant power de 7,
         // color -1, une position de départ de (0,6), special mouvement 2 et une
         // référence à l'échiquier sur lequel la pièce est placée.
@@ -40,7 +57,6 @@ public class Echiquier {
         Point piege1B = new Point(0, 2);
         Point piege2B = new Point(1, 3);
         Point piege3B = new Point(0, 4);
-        // TODO ajoute emplacement du trone point(0,3);
 
         //on ajoute les pièces du joueur 1 à la liste "player1"
 
@@ -70,7 +86,6 @@ public class Echiquier {
         Point piege1N = new Point(8, 4);
         Point piege2N = new Point(7, 3);
         Point piege3N = new Point(8, 2);
-        // TODO ajoute emplacement du trone point(0,3);
 
         player2.add(elephantN);
         player2.add(chienN);
@@ -176,20 +191,19 @@ public class Echiquier {
     //Cette méthode est utilisée pour changer le tour des joueurs. Elle bascule simplement entre les valeurs 1 et -1 pour identifier
     // le joueur en cours. Si le joueur actuel a perdu, la méthode affiche, un message indiquant qu'il a perdu et quitte le jeu.
     public void switchPlayer() {
-        this.turn = -this.turn;
+        this.turn = -this.turn+1;
         if (isLose()) {
             this.showEchiquier();
-            System.err.println("the player " + this.turn + " lose");
+            System.err.println("le joueur  " + this.turn + "  perdu");
             System.exit(0);
         }
     }
     //Cette méthode vérifie si un point est situé à l'intérieur du plateau de jeu. Elle prend en entrée un objet Point qui représente les coordonnées d'un point et retourne true si le point est situé à l'intérieur du plateau, et false sinon
     public boolean isPointInEchiquier(Point position) {
-        /**
-         * respecter les dimesntion du echiquier sinon declancher une exception dans
-         * piece
-         *
-         * @param un Point
+        /*
+          respecter les dimesntion du echiquier sinon declancher une exception dans
+          piece
+
          */
 
         return (position.getX() >= 0 && position.getX() <= 8) && (position.getY() >= 0 && position.getY() <= 6);
@@ -197,18 +211,17 @@ public class Echiquier {
 
     //Cette méthode vérifie si un point est situé sur une case d'eau. Elle prend en entrée un objet Point et retourne true si le point est situé sur une case d'eau, et false sinon.
     public boolean isPointWater(Point position) {
-        /**
-         * @param point savoir si une du water test suur la liste
-         */
+         // point savoir si une du water test suur la liste
+
 
         return listWaterPlace.contains(position);
     }
 
     //Cette méthode vérifie si un point est situé sur un piège. Elle prend en entrée un objet Point et retourne true si le point est situé sur un piège, et false sinon
     public boolean isPiege(Point posiPoint) {
-        /**
-         * @param un point test sur la list pour rendre power zero
-         */
+
+          //un point test sur la list pour rendre power zero
+
         if (turn == -1) {
             return listPiegePlayer2.contains(posiPoint);
         } else {
@@ -218,9 +231,9 @@ public class Echiquier {
 
     //Cette méthode vérifie si le joueur en cours a perdu. Elle vérifie si le joueur a encore des pièces et si le trône est toujours en sécurité. Si le joueur a perdu, la méthode retourne true, sinon elle retourne false
     public boolean isLose() {
-        /**
-         * pour chaque tour de switch user on va teste list ds pices ou la trone still
-         * existe
+        /*
+          pour chaque tour de switch user on va teste list ds pices ou la trone still
+          existe
          */
 
         if (turn == -1) {
@@ -244,15 +257,12 @@ public class Echiquier {
 
     //Cette méthode vérifie si le trône est en sécurité. Si le trône est occupé par une pièce ennemie ou s'il est situé sur une case d'eau, la méthode retourne false, sinon elle retourne true.
     public boolean isTroneGood() {
-        /**
-         * on va teste ou on va mettre un point si adversaire attient l'autre va perdu
-         * on va tester le point du trone is Vide pour chaque joueur
-         *
+        /*
+          on va teste ou on va mettre un point si adversaire attient l'autre va perdu
+          on va tester le point du trone is Vide pour chaque joueur
+
          */
         if (turn == -1) {
-            /**
-             * TODO met la point du trone pour chaque user
-             */
             return isPointVide(new Point(0, 3));
         } else {
             return isPointVide(new Point(8, 3));
@@ -262,9 +272,9 @@ public class Echiquier {
 
     //Cette méthode vérifie si un point donné est vide, c'est-à-dire s'il n'y a pas de pièce sur ce point. Elle prend en entrée un objet Point et retourne true si le point est vide, et false sinon.
     public boolean isPointVide(Point pointTest) {
-        /**
-         * @param Point si le point est vide apres l'iteration du listPiece et sa
-         *              position
+        /*
+          Point si le point est vide apres l'iteration du listPiece et sa
+                      position
          */
         boolean isVide = true;
         for (Piece point : player1) {
@@ -286,9 +296,9 @@ public class Echiquier {
 
     //Cette méthode vérifie si un point donné contient une pièce appartenant au joueur en cours. Elle prend en entrée un objet Point et retourne true si le point contient une pièce appartenant au joueur en cours, et false sinon
     public boolean isThereMyPiece(Point pointTest) {
-        /**
-         * @param Point si le point est vide apres l'iteration du listPiece et sa
-         *              position
+        /*
+         Point si le point est vide apres l'iteration du listPiece et sa
+                      position
          */
         boolean isVide = true;
         if (this.turn == -1) {
@@ -312,9 +322,9 @@ public class Echiquier {
 
     //Cette méthode choisit une pièce à partir de son emplacement sur le plateau. Elle prend en entrée un objet Point et retourne l'objet Piece correspondant à l'emplacement donné.
     public Piece choosePieceAt(Point point) {
-        /**
-         * pour chosir une piece pour faire les traimtment et sedeplacer sinon on
-         * declanche une exception s'i existe pas
+        /*
+          pour chosir une piece pour faire les traimtment et sedeplacer sinon on
+          declanche une exception s'i existe pas
          */
 
         Piece piece = null;
@@ -342,36 +352,20 @@ public class Echiquier {
 
     //Cette méthode supprime une pièce du plateau de jeu. Elle prend en entrée un objet Piece à supprimer
     public void removePiece(Piece pieceRmv) {
-        /**
-         * pour elemener dans unn deplacement du l'adversaire donc on va jouer sur le
-         * role
-         */
         /*
-         * if(this.turn==-1) { this.player2.remove(pieceRmv); } else {
-         * this.player1.remove(pieceRmv); }
+          pour elemener dans unn deplacement du l'adversaire donc on va jouer sur le
+          role
          */
         this.player1.remove(pieceRmv);
         this.player2.remove(pieceRmv);
 
     }
 
-    //Cette méthode met à jour la position des pièces sur le plateau.
-    public void updatePosition() {
-        /***
-         * pour faire la mise ajour de tout les pieces TODO pour afficher l'echquier et
-         * met la mise a jour
-         */
-    }
-
     //Cette méthode affiche le plateau de jeu sur la console
     public void showEchiquier() {
-        /**
-         * pour afficher la console TODO met la console d'affichage
-         */
-
-        /**
-         *
-         * String tes=""; tes=tes+" "; System.out.println(tes.substring(0,8));
+        /*
+          pour afficher la console
+          String tes=""; tes=tes+" "; System.out.println(tes.substring(0,8));
          */
 
         for (int j = 8; j >= 0; j--) {
@@ -383,22 +377,22 @@ public class Echiquier {
 
                 if ((j == 8 && i == 4) || (j == 7 && i == 3) || (j == 8 && i == 2)) {
 
-                    string="PN ";
+                    string="sanctuaire 2 ";
                 }
 
                 if ((j == 0 && i == 2) || (j == 1 && i == 3) || (j == 0 && i == 4)) {
 
-                    string="PB ";
+                    string="sanctuaire 1 ";
                 }
 
                 if (j == 0 && i == 3) {
 
-                    string="TB ";
+                    string="Trone 1 ";
                 }
 
                 if (j == 8 && i == 3) {
 
-                    string="TN ";
+                    string="Trone 2 ";
                 }
 
                 Piece p = this.getPieceAt(new Point(j, i));
@@ -414,16 +408,15 @@ public class Echiquier {
             }
             System.out.println();
             System.out.println("-----------------------------------------------------------------------------------------------------------------");
-            System.out.println();
-            // System.out.println("---------------------------------------------------------");
+            //System.out.println();
         }
-        System.out.println("*******piece Restant*******player -1 : "+this.player1.size()+" ************************player 1:"+this.player2.size()+"********************************************************");
+        System.out.println("******* Joueur -1 : "+this.player1.size()+" ******* Joueur 2 : "+this.player2.size()+" *******");
     }
 
     //permet de retourner la pièce qui est sur une case donnée
     public Piece getPieceAt(Point pointTest) {
-        /**
-         * @param point pour facilter traitement de deplacement
+        /*
+          point pour facilter traitement de deplacement
          */
 
         for (Piece it : player1) {
@@ -473,6 +466,63 @@ public class Echiquier {
             int pieceI = new Random().nextInt(this.player2.size());
             Piece randPiece = this.player2.get(pieceI);
             randPiece.randomMove();
+        }
+    }
+    public void enregistrerDeplacement(Piece piece, Point depart, Point arrivee) {
+        try {
+            // Établir une connexion à la base de données
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeu xou dou qi", "root", "");
+
+            // Préparer la requête SQL pour insérer le déplacement dans la base de données
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO deplacements (piece, depart_x, depart_y, arrivee_x, arrivee_y) VALUES (?, ?, ?, ?, ?)");
+            preparedStatement.setString(1, piece.pieceIcon().toString());
+            preparedStatement.setInt(2, depart.getX());
+            preparedStatement.setInt(3, depart.getY());
+            preparedStatement.setInt(4, arrivee.getX());
+            preparedStatement.setInt(5, arrivee.getY());
+
+            // Exécuter la requête SQL
+            preparedStatement.executeUpdate();
+
+
+            // Fermer la connexion à la base de données
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            logger.severe("Une exception s'est produite : " + e.getMessage());
+            e.printStackTrace();
+        } catch (SQLException e) {
+            logger.severe("Une exception s'est produite : " + e.getMessage());
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void Sauvegarderpartie(Piece piece, Point depart, Point arrivee) {
+
+        try {
+            // Établir une connexion à la base de données
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeu xou dou qi", "root", "");
+
+            // Préparer la requête SQL pour insérer le déplacement dans la base de données
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM deplacements ");
+            preparedStatement.setString(1, piece.pieceIcon().toString());
+            preparedStatement.setInt(2, depart.getX());
+            preparedStatement.setInt(3, depart.getY());
+            preparedStatement.setInt(4, arrivee.getX());
+            preparedStatement.setInt(5, arrivee.getY());
+
+            // Exécuter la requête SQL
+            preparedStatement.executeQuery();
+            // Fermer la connexion à la base de données
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            logger.severe("Une exception s'est produite : " + e.getMessage());
+            e.printStackTrace();
+        } catch (SQLException e) {
+            logger.severe("Une exception s'est produite : " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
